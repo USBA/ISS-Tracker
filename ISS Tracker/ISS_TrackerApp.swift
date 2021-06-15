3e3a4eb3a9a32c4cc554afe297516b5197f7449f
Umayanga Alahakoon <umayanga@usa.com>
2021-06-15 08:13:55 +0530

    Initial Commit

---
+++ ISS Tracker/ISS_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISS_TrackerApp.swift
+//  ISS Tracker
+//
+//  Created by Umayanga Alahakoon on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct ISS_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
